a8ee0d4f257caefb53b5ce8a651ac181d518b121
Vlad Hristov <vpchar@gmail.com>
2022-12-16 21:58:34 +0200

    exercise 2 -- my first test suite for node ;-)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let retStr = '';
+    if(num<0)return('ERROR');
+    for(i=0;i<num;i++){
+        retStr+=str;
+    }
+    return retStr;
 };
 
 // Do not edit below this line
