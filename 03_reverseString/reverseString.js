4b66d96bf49644800f7a3baca324da96a5f6c596
Vlad Hristov <vpchar@gmail.com>
2022-12-16 22:08:00 +0200

    Exercise 3 -- done [pass]

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inpString = '') {
+    let res = '';
+    for(i=inpString.length-1;i>=0;i--){
+        res += inpString.charAt(i);
+    }
+    return res;
 };
 
 // Do not edit below this line
