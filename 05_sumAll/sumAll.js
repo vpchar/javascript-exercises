1cc589496037166a72716e5ef32f8267abf5f32e
Vlad Hristov <vpchar@gmail.com>
2022-12-17 07:25:19 +0200

    test 05 done; added more tests to spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    //check input
+    if(num1 == NaN || num2 == NaN 
+        || num1 <0 || num2<0
+        || !Number.isInteger(num1)
+        || !Number.isInteger(num2)
+    )return('ERROR');
+    // get bigger num
+    let bigger = 0, smaller = 0;
+    if(num1>num2){
+        bigger = num1;
+        smaller= num2;
+    } else if(num2>num1){
+        bigger = num2;
+        smaller=num1;
+    } else {
+        //bigger = smaller = num1;
+        return(num1);
+    }
+    //loop and add numbers
+    let result = 0;
+    for(i=smaller;i<=bigger;i++){
+        result += i;
+    }
+    return(result);
 };
 
 // Do not edit below this line
