3232eb7b4a6599409d8449d3aacf18c6092b52ad
Vlad Hristov <vpchar@gmail.com>
2022-12-17 08:17:00 +0200

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let tempCelsius = 0;
+  tempCelsius = tempFahrenheit - 32;
+  tempCelsius *= 5/9;
+  tempCelsius= Math.round(tempCelsius * 10) / 10
+
+  return tempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let tempFahrenheit = 0;
+  tempFahrenheit = tempCelsius * (9/5);
+  tempFahrenheit += 32;
+  tempFahrenheit= Math.round(tempFahrenheit * 10) / 10
+
+  return tempFahrenheit;
 };
 
 // Do not edit below this line
