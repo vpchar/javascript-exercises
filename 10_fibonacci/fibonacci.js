8b0f8f7946e97e236507744174ca1843a3a711d6
Vlad Hristov <vpchar@gmail.com>
2022-12-21 18:25:20 +0200

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    return n < 0 ? 'OOPS'
+        : n < 1 ? 0
+        : n >0 && n< 2 ? 1
+        : fibonacci(n - 1) + fibonacci(n - 2)
 };
 
 // Do not edit below this line
