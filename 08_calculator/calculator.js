c0ac53d084d4428b6d739ff03229c541fd80907c
Vlad Hristov <vpchar@gmail.com>
2022-12-21 17:35:35 +0200

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,50 @@
-const add = function() {
-	
+const add = (a,b) =>	a+b;
+
+const subtract = (a,b) => a-b;
+
+const sum = a => { //I HATE ES6 -- this is a virus!
+  let sum =0; 
+  a.forEach(element => {
+     sum += element;
+  });
+  return sum;
+}
+
+const multiply = function(a) { //back to the old ways...
+  let total = 1;
+  a.forEach(element => {
+    total *= element
+  });
+  return total;
 };
 
-const subtract = function() {
-	
-};
+const power = (a,b) => a ** b; //but we MUST be RELEVANT!
 
-const sum = function() {
-	
-};
+//...so at the end...
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+/* Started with:
+const factorial = f =>{
+  if (f < 0) 
+    return -1;
+  else if  (f == 0) 
+    return 1;
+  else {
+   return (f * factorial(f - 1));
+  }
+  
+}
+// old school cpp coder here: tell me, HOW is this cleaner? 
+// anyway, I'll at least write it 
+// in human readable form:
+const factorial = f => 
+  f<0 
+    ? -1 
+    : f==0 
+      ? 1 
+      : (f * factorial(f - 1)); 
+*/
+//and that's what you show to the HR department:
+const factorial = f => f<0 ? -1 : f==0 ? 1 : f * factorial(f - 1); 
 
 // Do not edit below this line
 module.exports = {
