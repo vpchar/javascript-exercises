2489253c97c2ff37421292e4ab4fc5c235b4ed8d
Vlad Hristov <vpchar@gmail.com>
2022-12-21 19:06:47 +0200

    oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce( (oldestAuthor, currentAutor) => {
+        //console.log(oldestAuthor);
+        (currentAutor.yearOfDeath) 
+            ? currentAutor.yearOfDeath 
+            : currentAutor.yearOfDeath = new Date().getFullYear() ;
+        return ((oldestAuthor.yearOfDeath-oldestAuthor.yearOfBirth) > 
+                (currentAutor.yearOfDeath-currentAutor.yearOfBirth))
+                    ? oldestAuthor 
+                    : currentAutor
+    },{})
 };
 
 // Do not edit below this line
