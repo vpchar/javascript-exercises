8428e16f9702e7b95fe3658e875410584bb347e0
Vlad Hristov <vpchar@gmail.com>
2022-12-21 18:04:07 +0200

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
 
+const palindromes = string => {
+    string = string.toUpperCase().replace(/[^A-Za-z]/g,'');
+    //console.log(string);
+    const len = string.length-1;
+    for(i=0;i<string.length/2;i++){
+        //loop from start, compare from end --
+        if (string[i] !== string[len-i]) return false; //test failed
+    }
+    //else
+    return true; //its palindrome
 };
 
 // Do not edit below this line
